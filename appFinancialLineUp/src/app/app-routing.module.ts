1b3bbe515f25e6e263b70af2d87fb2f4917b4924
Arnold.Abraham <Arnold.Abraham@avl.com>
2020-06-11 19:41:10 +0200

    implements first route for lifestyles-component.

--- appFinancialLineUp/src/app/app-routing.module.ts
+++ appFinancialLineUp/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LifestylesComponent} from "./lifestyles/lifestyles/lifestyles.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'lifestyles', component: LifestylesComponent
+},
+  {
+    path: '**',
+    redirectTo: 'lifestyles',
+    pathMatch: 'prefix'
+  },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
